280729af45c7cde306aec3d90e2b05e510b108a5
Documenter.jl <documenter@juliadocs.github.io>
2021-12-23 09:50:15 +0000

    build based on 7013034

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DFUtil.jl","page":"DFUtil.jl","title":"DFUtil.jl","text":"","category":"section"},{"location":"","page":"DFUtil.jl","title":"DFUtil.jl","text":"Documentation for DFUtil.jl","category":"page"},{"location":"","page":"DFUtil.jl","title":"DFUtil.jl","text":"","category":"page"}]
+}
