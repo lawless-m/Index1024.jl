eb1eb0a8c1f12b6167ed3df471fe3d9f7e45e08e
lawless-m <matt-github@steponnopets.net>
2022-01-04 10:36:30 +0000

    time to expand the leaf page

--- src/Index1024.jl
+++ src/Index1024.jl
@@ -192,7 +192,7 @@ function write_pages(io, sorted_keys, kvs, leaf_tag)
         next_kvs[sks[end]] = position(io)
         root = build_page(sks, kvs, leaf_tag)
         s = write(io, root)
-        #println(stderr, "Nodes size $s")
+        println(stderr, "Nodes size $s")
     end
     next_sorted_keys, next_kvs
 end
